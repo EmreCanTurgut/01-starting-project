c8a29bca485a4721728df45ffc99438f42140d14
EmreCanTurgut <emrecanturgut3@gmail.com>
2024-10-11 23:08:48 +0300

    tabbuttonCss

--- src/App.jsx
+++ src/App.jsx
@@ -37,12 +37,12 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleSelect("components")}>
+            <TabButton isSelected={selectedTopic==="components"} onSelect={() => handleSelect("components")}>
               Components
             </TabButton>
-            <TabButton onSelect={handleSelect.bind(this, "jsx")}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
+            <TabButton isSelected={selectedTopic==="jsx"} onSelect={handleSelect.bind(this, "jsx")}>JSX</TabButton>
+            <TabButton isSelected={selectedTopic==="props"} onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton isSelected={selectedTopic==="state"} onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
           {!selectedTopic ? (
             <p>please select a topic</p>
